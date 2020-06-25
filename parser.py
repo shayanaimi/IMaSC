55c16a1ba4761351210ae4733e4ca3697f537e2c
Pavle Medvidovic <medvidov@usc.edu>
2020-06-25 16:26:07 -0700

    Figured out how to get each field for the JSON.

--- parser.py
+++ parser.py
@@ -2,18 +2,32 @@ import json
 
 # get the JSON from mls_pubs.json
 data = []
+text = ''
+
 with open('data/microwave_limb_sounder/mls_pubs.json') as f:
     for line in f:
-            data.append(json.loads(line))
+            j = json.loads(line)
+            data.append(j)
+            # print(j.get("_source").get("text"))
+            break
+
+text = data[0].get("_source").get("text")
+print(text)
+
+# check that data has all the lines
+# print(len(data))
+# shocker, it does
 
 # print first line
 # change this code because you realize that Python has 0 indexing
 # print(data[0])
 
+
 # save first line to a .txt file
-#line = open('line.txt','x+')
+# line = open('line.txt','x+')
+# deprecated, need to dump to a json file. Should have added this comment earlier
 
-with open('line.json','w+') as line:
-    json.dump(data[2],line)
+# with open('line.json','w+') as line:
+#     json.dump(data[2],line)
 
-print(data[2])
+# print(data[0]['text'])
