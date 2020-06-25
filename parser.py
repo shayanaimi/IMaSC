78cd97eb819acc15fd8eca2d6e994c7e116fa109
Pavle Medvidovic <medvidov@usc.edu>
2020-06-24 21:02:55 -0700

    Added a line to just write the JSON to a .txt file. Need to delete the file though.

--- parser.py
+++ parser.py
@@ -1,10 +1,17 @@
 import json
 
-# Get the JSON from mls_pubs.json
+# get the JSON from mls_pubs.json
 data = []
 with open('data/microwave_limb_sounder/mls_pubs.json') as f:
     for line in f:
             data.append(json.loads(line))
 
-# Print first line
-print(data[1])
+# print first line
+# change this code because you realize that Python has 0 indexing
+print(data[0])
+
+# save first line to a .txt file
+#line = open('line.txt','x+')
+
+with open('line.txt','x+') as line:
+    line.write('' + data[0])
