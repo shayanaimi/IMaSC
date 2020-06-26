831ef24f51135a0bbd0c997c16c59263688af13a
Pavle Medvidovic <medvidov@usc.edu>
2020-06-26 15:51:51 -0700

    Trying to format data for use with Prodigy.

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 import json
-import spacy
+# import spacy
 from sys import argv
 
 script, json_file = argv
 
 # Get the article texts from mls_pubs.json
 titles = []
-articles = []
+articles = {}
+articles['text'] = []
 
 # Opens the file and iterates through each line
 # Adds titles and article texts to dicts
@@ -14,30 +15,33 @@ source = open(json_file)
 for line in source:
         j = json.loads(line)
         titles.append(j.get("_source").get("title"))
-        articles.append(j.get("_source").get("text"))
+        articles['text'].append(j.get("_source").get("text"))
             
 # Save it all to a text file
-# file = open('articles.json','w+')
-# for i in text:
-#     file.(i)
+file = open('article_text.jsonl','w+')
+json.dump(articles, file)
 
-# file.close()
+file.close()
 # Commenting out, may not need to save to a file of any sort
 # May need to add code to save texts to a .txt, so keeping
 
-# Load English tokenizer, tagger, parser, NER and word vectors
-nlp = spacy.load("en_core_web_sm")
 
-# Process whole documents
-text = articles[0]
-doc = nlp(text)
 
-# Analyze syntax
-print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
-print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 
-# Find named entities, phrases and concepts
-for entity in doc.ents:
-    print(entity.text, entity.label_)
+# Commenting out spaCy code, actually need to parse the jsonl
+# # Load English tokenizer, tagger, parser, NER and word vectors
+# nlp = spacy.load("en_core_web_sm")
 
-# Adapted from spaCy website
+# # Process whole documents
+# text = articles[0]
+# doc = nlp(text)
+
+# # Analyze syntax
+# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+# # Find named entities, phrases and concepts
+# for entity in doc.ents:
+#     print(entity.text, entity.label_)
+
+# # Adapted from spaCy website
