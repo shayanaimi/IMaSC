edbf062b7b4e32c0cafd12e07717537d6a3426c6
Pavle Medvidovic <medvidov@usc.edu>
2020-07-07 03:42:04 -0700

    Adding shuffler and resulting text files. Need to figure out what to do with regards to large files. May also need to combine parser and shuffler.

--- shuffler.py
+++ shuffler.py
@@ -1,11 +1,11 @@
 import random
 
-# article_text = original parsed article text file
-# shuffled_text = file where we will write the shuffled text
+# article_text is the original parsed article text file
+# The sets are my training, validation, and testing sets for Prodigy
 def shuffle_article_text(
     article_text="data/microwave_limb_sounder/article_text.jsonl",
-    shuffled_text="data/microwave_limb_sounder/shuffle_text.jsonl",
-    training_set="data/microwave_limb_sounder/training_set.jsonl",
+    training_set_1="data/microwave_limb_sounder/training_set_1.jsonl",
+    training_set_2="data/microwave_limb_sounder/training_set_2.jsonl",
     validation_set="data/microwave_limb_sounder/validation_set.jsonl",
     testing_set="data/microwave_limb_sounder/testing_set.jsonl"
 ):
@@ -18,50 +18,28 @@ def shuffle_article_text(
     # Sorts the data (not entirely sure why this works, but hey)
     data.sort()
 
-    print(len(data)*0.7)
-    count = 0
+    # Writes 70% of shuffled text to training set and 15% each to validation
+    # and testing sets (70 + 15 + 15 = 100)
+    training_1 = open(training_set_1, "w+")
+    training_2 = open(training_set_2, "w+")
+    validation = open(validation_set, "w+")
+    testing = open(testing_set, "w+")
 
-    for j in range(len(data) + 1):
-        #print(data[j])
-        count += 1
-        #print(count)
+    for j in range(len(data)):
 
-    # Writes 70% of shuffled text to training set
-    training = open(training_set)
-    validation = open(validation_set)
-    testing = open(test_set)
-    with open(training_set,'w+') as target:
-        for j in range(len(data) + 1):
-            # print(i)
-            # break
-            if j < len(data)*0.7:
-                target.write(data[j][1])
-                # print("Test")
-                # print(j)
-                # break
-            elif j >= len(data*0.7) and j < len(data)*0.85:
-                target.write(data[j][1])
-            else:
-              target.write(data[j][1])  
-            
-    target.close()
-
-    # Writes 15% of shuffled text to training set
-    # with open(validation_set,'w+') as target:
-    #     for i,line in data:
-    #         if i > len(data)*0.7 and i < len(data)*0.85:
-    #             target.write(line)
-    #         else:
-    #             break
-    # target.close()
-
-    # # Writes 15% of shuffled text to training set
-    # with open(test_set,'w+') as target:
-    #     for i,line in data:
-    #         if i >= len(data)*0.85:
-    #             break
-    #         target.write(line)
-    # target.close()
+        if j < len(data)*0.35:
+            training_1.write(data[j][1])
+        elif j >= len(data)*0.35 and j < len(data)*0.7:
+            training_2.write(data[j][1])
+        elif j >= len(data)*0.7 and j < len(data)*0.85:
+            validation.write(data[j][1])
+        else:
+            testing.write(data[j][1])  
+        
+    training_1.close()
+    training_2.close()
+    validation.close()
+    testing.close()
 
 # Executes the script
 if __name__ == "__main__":
