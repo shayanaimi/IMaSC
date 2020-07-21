6b609abf990724b92c893186aa6a4738ae831baf
Pavle Medvidovic <medvidov@usc.edu>
2020-07-20 21:54:30 -0700

    Cleaned parser.py. All escapes and backslashes are now removed.

--- parser.py
+++ parser.py
@@ -45,8 +45,7 @@ def get_article_text(
             text = remove_escapes(i)
 
             # Clean the strings of other escapes and dump to temporary output file
-            cleaned_string = clean_text(text)
-            articles["text"] = cleaned_string
+            articles["text"] = text
             json.dump(articles, output)
             output.write("\n")
 
@@ -98,68 +97,22 @@ def sortByLength(o):
 
 def remove_escapes(text):
     # Remove unicode escapes
-    unicode_remove = re.compile(r"\\[uU]([a-zA-Z0-9_]{4})")
+    text = text.replace("-\n", "")  # word broken over line break
+    unicode_remove = re.compile(r"\\[uU]([a-zA-Z0-9]{4})")
     text = re.sub(unicode_remove, "", text.encode('unicode_escape').decode())
 
     # Remove hex escapes
-    hex_remove = re.compile(r"\\[xX]([a-zA-Z0-9_]{2})")
+    hex_remove = re.compile(r"\\[xX]([a-zA-Z0-9]{2})")
     text = re.sub(hex_remove, "", text)
 
     # Removes \n and other escapes
-    text = re.sub(r"\\[a-zA-Z]", " ", text) # whatever \\n is
+    text = re.sub(r"\\[a-zA-Z.]", " ", text) # whatever \\n is
     text = re.sub(r"\\R", " ", text) # Should cover all line breaks
     text = re.sub(r"\"", "", text) # Exact match for whatever \" is
+    text = re.sub(r"\\", "", text) # Exact match for whatever \" is
 
     return text
-
-def clean_text(text):
-        # simple text cleaning to remove some unicode representations
-        text = text.replace("-\n", "")  # word broken over line break
-        text = text.replace("\u00a0", "")  # empty space
-        text = text.replace("\u2022", "; ")  # bullet
-        text = text.replace("\u201c", "'")  # left double quotation mark
-        text = text.replace("\u201d", "'")  # right double quotation mark
-        text = text.replace("\u03b7", "GREEK_ETA")  # greek eta
-        text = text.replace("\u03b1", "GREEK_ALPHA")  # greek alpha
-        text = text.replace("\u03b3", "GREEK_GAMMA")  # greek eta
-        text = text.replace("\u2212", "-")  # minus sign
-        text = text.replace("\u002B", "+")  # plus sign
-        text = text.replace("\u2206", "INCREMENT")  # increment
-        text = text.replace("\u2264", "<=")  # less than equal to
-        text = text.replace("\u2265", ">=")  # greater than equal to
-        text = text.replace("\u2013", "-")  # dash
-        text = text.replace("\u03b5", "GREEK_EPSILON")  # greek small letter epsilon
-        text = text.replace("\u03a6", "GREEK_PHI")  # greek phi
-        text = text.replace("\u2026", "...")  # horizontal elipsis
-        text = text.replace("\u2018", "'")  # right single quote
-        text = text.replace("\u2019", "'")  # left single quote
-        text = text.replace("\u03c4", "GREEK_TAU")  # greek tau
-        text = text.replace("\u00b5", "MICRO_SIGN")  # greek tau
-        text = text.replace("\u00b5", "GREEK_LAMBDA")  # greek lambda
-        text = text.replace("\u03c3", "GREEK_SIGMA")  # greek sigma
-        text = text.replace("\u20ac", "EURO")  # greek sigma
-        text = text.replace("\u2215", "/")  # division slash
-        text = text.replace("\u00e4", "ae")  # a umlaut
-        text = text.replace("\u00e8", "e")  # small latin e
-        text = text.replace("\u25e6", "")  # bullet
-        text = text.replace("\u00b1", "+-")  # plus minus sign
-        text = text.replace("\u2032", "PRIME")  # prime
-        text = text.replace("\ud835", " ")  # invalid character
-        text = text.replace("\udf0e", " ")  # invalid character
-        text = text.replace("\u00a9", "COPYRIGHT")  # copyright
-        text = text.replace("\u2191", "UP_ARROW")  # arrow
-        text = text.replace("\u2192", "RIGHT_ARROW")  # arrow
-        text = text.replace("\u2190", "LEFT_ARROW")  # arrow
-        text = text.replace("\u2193", "DOWN_ARROW")  # arrow
-        text = text.replace("\u2194", "LEFT_RIGHT_ARROW")  # arrow
-        text = text.replace("\u00a0", "_")  # no break space
-        text = text.replace("\u0000", "")  # null character
-        text = text.replace("\u00b0", " degrees")  # degrees
-        text = text.replace("\\\\", "") # get rid of random backslashes
-        text = text.replace("\\\"", "") # get rid of random quotes
-
-        return text
-
+    
 # Executes the script
 if __name__ == "__main__":
     get_article_text()
