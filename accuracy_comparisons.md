d55a2e150c434e54dea6c90da9e5bbfbf698dc30
Pavle Medvidovic <medvidov@usc.edu>
2020-08-05 10:05:17 -0700

    Added functionality to save visualizations to PDF files.

--- accuracy_comparisons.md
+++ accuracy_comparisons.md
@@ -1,7 +1,11 @@
 |   | INSTRUMENT  | SPACECRAFT  | Both  |
 |---|---|---|---|
-| Curve training  | 0.63  | 0.62  | 0.63  |
+| NUmber of annotations  | 108  | 32  | 140  |
 | Batch training  | 0.657  | 0.594  | 0.636  |
+| Curve training  | 0.63  | 0.62  | 0.63  |
+|   |
+| Batch training  | 0.593  | 0.562  | 0.564  |
+
 
 Note: Accuracy decreases in the last 25% for INSTRUMENT and when training both. It increases when training SPACECRAFT.
 This is using the en_core_web_sm model.
@@ -11,4 +15,3 @@ Using a blank:en model, we get the following:
 
 |   | INSTRUMENT  | SPACECRAFT  | Both  |
 |---|---|---|---|
-| Batch training  | 0.593  | 0.562  | 0.564  |
--- visualize.py
+++ visualize.py
@@ -1,12 +1,16 @@
 import spacy
 from spacy import displacy
+import pdfkit
+
 
 colors = {"SPACECRAFT": "#b35f50", "INSTRUMENT":"#509cb3"}
-print(type(colors))
 options = {"ents": ["SPACECRAFT","INSTRUMENT"], "colors": colors}
 
 text = "We will show with scatter diagrams of water vapor and ozone mixing ratios from the balloon soundings that there are signicant seasonal differences in the contributions from wave, source, and path variability. We augment the analysis by comparing the variance in the balloon soundings to simulated proles constructed from water vapor and ozone data from the Aura Microwave Limb Sounder (MLS) using a new reverse domain lling technique."
 
 nlp = spacy.load("models")
 doc = nlp(text)
-displacy.serve(doc, style="ent", options=options)
+# displacy.serve(doc, style="ent", options=options)
+html = displacy.render(doc, style="ent", options=options)
+pdfkit.from_string(html,'test.pdf')
+
