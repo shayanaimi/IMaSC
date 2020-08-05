34148e7d75826ee4bc0ee635ef6cae394c12be01
Pavle Medvidovic <medvidov@usc.edu>
2020-08-05 08:12:02 -0700

    Added reports and updated visualizer colors.

--- visualize.py
+++ visualize.py
@@ -1,9 +1,9 @@
 import spacy
 from spacy import displacy
 
-colors = {"SPACECRAFT": "linear-gradient(90deg, #aa9cfc, #fc9ce7)"}
+colors = {"SPACECRAFT": "#b35f50", "INSTRUMENT":"#509cb3"}
 print(type(colors))
-options = {"ents": ["SPACECRAFT"], "colors": colors}
+options = {"ents": ["SPACECRAFT","INSTRUMENT"], "colors": colors}
 
 text = "We will show with scatter diagrams of water vapor and ozone mixing ratios from the balloon soundings that there are signicant seasonal differences in the contributions from wave, source, and path variability. We augment the analysis by comparing the variance in the balloon soundings to simulated proles constructed from water vapor and ozone data from the Aura Microwave Limb Sounder (MLS) using a new reverse domain lling technique."
 
