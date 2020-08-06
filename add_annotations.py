44dbf43ef6b7b4f630791d6b8132581792537645
Pavle Medvidovic <medvidov@usc.edu>
2020-08-06 09:47:58 -0700

    Adding code to read text and add existing annotations. Testing correct JSON format.

---
+++ add_annotations.py
@@ -0,0 +1,15 @@
+import json
+
+spans = []
+item = {}
+item["start"] = 0
+item["end"] = 0
+item["token_start"] = 0
+item["token_end"] = 0
+item["label"] = ""
+
+spans.append(item)
+spans.append(item)
+
+
+print(spans)
