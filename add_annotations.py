cad00bbb3186d46c9916075ff43470b9ba4f4b47
Pavle Medvidovic <medvidov@usc.edu>
2020-08-08 14:04:32 -0700

    Planning process to manually insert annotations

--- add_annotations.py
+++ add_annotations.py
@@ -8,6 +8,20 @@ item["token_start"] = 0
 item["token_end"] = 0
 item["label"] = ""
 
+instruments = ["MLS", "Microwave Limb Sounder", "SMR", "Sub-Millimetre Radiometer", 
+"ACE-FTS", "Fourier Transform Ultra-Violet Spectrometer", "SCIAMACHY"]
+spacecraft = []
+models = []
+experiments = ["ACE","Atmospheric Chemistry Experiment"]
+locations = []
+
+#Take in data from annotations - Copy.jsonl
+#for each JSON
+# Check if we have spans
+# if yes, spans = spans, if no initialize spans
+#text = JSON text
+# Substring checking
+
 spans.append(item)
 spans.append(item)
 
